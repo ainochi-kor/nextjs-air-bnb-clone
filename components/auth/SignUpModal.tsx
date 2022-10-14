c4d7ee1c036d56535ec454444db14649b88e179e
ainochi-kor <kthrkdals@gmail.com>
2022-10-14 20:12:00 +0900

    add: Sing Up Modal

--- components/auth/SignUpModal.tsx
+++ components/auth/SignUpModal.tsx
@@ -20,12 +20,51 @@ const Container = styled.div`
   }
 
   .input-wrapper {
-    p
+    position: relative;
+    margin-bottom: 16px;
+    input {
+      position: relative;
+      width: 100%;
+      height: 46px;
+      padding: 0 44px 0 11px;
+      border: 1px solid ${palette.gray_eb};
+      border-radius: 4px;
+      font-size: 16px;
+      outline: none;
+      ::placeholder {
+        color: ${palette.gray_76};
+      }
+    }
+    svg {
+      position: absolute;
+      right: 11px;
+      top: 16px;
+    }
   }
 `;
 
 const SignUpModal: React.FC = () => {
-  return <Container>Sign Up</Container>;
+  return (
+    <Container>
+      <CloseXIcon className="mordal-close-x-icon" />
+      <div className="input-wrapper">
+        <input placeholder="이메일 주소" type="email" name="email" />
+        <MailIcon />
+      </div>
+      <div className="input-wrapper">
+        <input placeholder="이름(예:길동)" />
+        <PersonIcon />
+      </div>
+      <div className="input-wrapper">
+        <input placeholder="성(예: 홍)" />
+        <PersonIcon />
+      </div>
+      <div className="input-wrapper">
+        <input placeholder="비밀번호 설정하기" type="password" />
+        <OpenedEyeIcon />
+      </div>
+    </Container>
+  );
 };
 
 export default SignUpModal;
