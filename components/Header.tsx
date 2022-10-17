e81f09e9209eeb2af193e1e8a9f8dc66f80d5657
ainochi-kor <kthrkdals@gmail.com>
2022-10-18 00:31:49 +0900

    wip: added AuthModal, PassowrdWarning ...

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import useModal from "../hooks/useModal";
 import AirbnbLogoIcon from "../public/static/svg/logo/logo.svg";
 import AirbnbLogoTextIcon from "../public/static/svg/logo/logo_text.svg";
+import { useSelector } from "../store";
 import palette from "../styles/palette";
 import SignUpModal from "./auth/SignUpModal";
 import ModalPortal from "./MordalPortal";
+import HamburgerIcon from "../public/static/svg/header/hamburger.svg";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store/auth";
 
 const Container = styled.div`
   position: sticky;
@@ -78,10 +83,38 @@ const Container = styled.div`
       z-index: 11;
     }
   }
-`;
 
+  .header-user-profile {
+    display: flex;
+    align-items: center;
+    height: 42px;
+    padding: 0 6px 0 16px;
+    border: 0;
+    box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.18);
+    border-radius: 21px;
+    background-color: white;
+    cursor: pointer;
+    outline: none;
+    &:hover {
+      box-shadow: 0px 2px 8px rgba(0, 0, 0.12);
+    }
+
+    .header-user-profile-image {
+      margin-left: 8px;
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+    }
+  }
+`;
+ModalPortal;
 const Header: React.FC = () => {
   const [modalOpened, setModalOpened] = useState(false);
+  const { openModal, ModalPortal, closeModal } = useModal();
+  const user = useSelector((state) => state.user);
+
+  const dispatch = useDispatch();
+
   return (
     <Container>
       <div className="header-logo-wrapper">
@@ -92,18 +125,51 @@ const Header: React.FC = () => {
         <button
           type="button"
           className="header-sign-up-button"
-          onClick={() => setModalOpened(true)}
+          onClick={() => {
+            dispatch(authActions.setAuthMode("signup"));
+            openModal();
+          }}
         >
           회원가입
         </button>
-        <button type="button" className="header-login-button">
+        <button
+          type="button"
+          className="header-login-button"
+          onClick={() => {
+            dispatch(authActions.setAuthMode("login"));
+            openModal();
+          }}
+        >
           로그인
         </button>
       </div>
       {modalOpened && (
-        <ModalPortal closePortal={() => setModalOpened(false)}>
-          <SignUpModal />
+        <ModalPortal>
+          <SignUpModal closeModal={closeModal} />
         </ModalPortal>
+      )}
+      {user.isLogged ? (
+        <button className="header-user-profile" type="button">
+          <HamburgerIcon />
+          <img
+            src={user.profileImage}
+            className="header-user-profile-image"
+            alt=""
+          />
+        </button>
+      ) : (
+        <div className="header-auth-buttons">
+          <button
+            type="button"
+            className="header-sign-up-button"
+            onClick={openModal}
+          >
+            회원가입
+          </button>
+          <button type="button" className="header-login-button">
+            로그인
+          </button>
+        </div>
       )}
     </Container>
   );
--- components/auth/SignUpModal.tsx
+++ components/auth/SignUpModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import styled from "styled-components";
 import CloseXIcon from "../../public/static/svg/modal/modal_colose_x_icon.svg";
 import MailIcon from "../../public/static/svg/auth/mail.svg";
@@ -15,84 +15,15 @@ import { useDispatch } from "react-redux";
 import { userActions } from "../../store/user";
 import { commonActions } from "../../store/common";
 import useValidateMode from "../../hooks/useValidateMode";
+import PasswordWarning from "./PasswordWarning";
 
-const Container = styled.div`
-  width: 568px;
-  padding: 32px;
-  background-color: white;
-  z-index: 11;
+const PASSWORD_MIN_LENGTH = 8;
 
-  .mordal-close-x-icon {
-    cursor: pointer;
-    display: block;
-    margin: 0 0 40px auto;
-  }
+interface IProps {
+  closeModal: () => void;
+}
 
-  .input-wrapper {
-    position: relative;
-    margin-bottom: 16px;
-    input {
-      position: relative;
-      width: 100%;
-      height: 46px;
-      padding: 0 44px 0 11px;
-      border: 1px solid ${palette.gray_eb};
-      border-radius: 4px;
-      font-size: 16px;
-      outline: none;
-      ::placeholder {
-        color: ${palette.gray_76};
-      }
-    }
-    svg {
-      position: absolute;
-      right: 11px;
-      top: 16px;
-    }
-  }
-
-  .sign-up-password-input-wrapper {
-    svg {
-      cursor: pointer;
-    }
-  }
-
-  .sign-up-birthdat-label {
-    font-size: 16px;
-    font-weight: 600;
-    margin-top: 16px;
-    margin-bottom: 8px;
-  }
-
-  .sign-up-modal-birthday-info {
-    margin-bottom: 16px;
-    color: ${palette.charcoal};
-  }
-
-  .sign-up-modal-birthday-selectors {
-    display: flex;
-    margin-bottom: 24px;
-    .sign-up-modal-birthday-month-selector {
-      margin-rignt: 16px;
-      flex-grow: 1;
-    }
-    .sign-up-modal-birthday-day-selector {
-      margin-right: 16px;
-      width: 25%;
-    }
-    .sign-up-modal-birthday-year-selector {
-      width: 33.3333%;
-    }
-  }
-
-  .sign-up-modal-submit-button-wrapper {
-    margin-bottom: 16px;
-    padding-bottom: 16px;
-    border-bottom: 1px solid ${palette.gray_eb};
-  }
-`;
-
-const SignUpModal: React.FC = () => {
+const SignUpModal: React.FC<IProps> = ({ closeModal }) => {
   const [email, setEmail] = useState("");
   const [lastname, setLastname] = useState("");
   const [firstname, setFirstname] = useState("");
@@ -101,6 +32,7 @@ const SignUpModal: React.FC = () => {
   const [birthYear, setBirthYaer] = useState<string | undefined>();
   const [birthDay, setBirthDay] = useState<string | undefined>();
   const [birthMonth, setBirthMonth] = useState<string | undefined>();
+  const [passwordFocused, setPasswordFocused] = useState(false);
 
   const dispatch = useDispatch();
   const { setValidateMode } = useValidateMode();
@@ -176,9 +108,92 @@ const SignUpModal: React.FC = () => {
     // }
   };
 
+  //* 비밀번호 인풋 포커스 되었을 때
+  const onFocusPassword = () => {
+    setPasswordFocused(true);
+  };
+
+  //* password가 이름이나 이메일을 포함하는지
+  const isPasswordHasNameOrEmail = useMemo(
+    () =>
+      !password ||
+      !lastname ||
+      password.includes(lastname) ||
+      password.includes(email.split("@")[0]),
+    [password, lastname, email]
+  );
+
+  //* 비밀번호가 최소 자릿수 이상인지
+  const isPasswordOverMinLength = useMemo(
+    () => !!password && password.length >= PASSWORD_MIN_LENGTH,
+    [password]
+  );
+
+  //* 비밀번호가 숫자나 특수기호를 포함하는지
+  const isPasswordHasNumberOrSymbol = useMemo(
+    () =>
+      !(
+        /[{}[\]/?.,;:|)*~`!^\-_+<>@#$%&\\=('"]/g.test(password) ||
+        /[0-9]/g.test(password)
+      ),
+    [password]
+  );
+
+  //* 회원가입 폼 입력 값 확인하기
+  const validateSignUpForm = () => {
+    //* 인풋 값이 없다면
+    if (!email || !lastname || !firstname || !password) {
+      return false;
+    }
+    //* 비밀번호가 올바르지 않다면
+    if (
+      isPasswordHasNameOrEmail ||
+      !isPasswordOverMinLength ||
+      isPasswordHasNumberOrSymbol
+    ) {
+      return false;
+    }
+
+    //* 회원가입 폼 제출하기
+    const onSubmitSignUp = async (evnet: React.FormEvent<HTMLFormElement>) => {
+      evnet.preventDefault();
+
+      setValidateMode(true);
+      console.log(validateSignUpForm());
+
+      if (validateSignUpForm()) {
+        try {
+          const signUpBody = {
+            email,
+            lastname,
+            firstname,
+            password,
+            birthday: new Date(
+              `${birthYear}-${birthMonth!.replace("월", "")}-${birthDay}`
+            ).toISOString(),
+          };
+          const { data } = await signupAPI(signUpBody);
+          dispatch(userActions.setLoggedUser(data));
+          console.log(data);
+
+          dispatch(userActions.setLoggedUser(data));
+          closeModal();
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    };
+  };
+
+  useEffect(() => {
+    return () => {
+      setValidateMode(false);
+    };
+  }, []);
+
   return (
     <Container onSubmit={onSubmitSignUp}>
-      <CloseXIcon className="mordal-close-x-icon" />
+      <CloseXIcon className="mordal-close-x-icon" onClick={closeModal} />
       <div className="input-wrapper">
         <Input
           placeholder="이메일 주소"
@@ -218,8 +233,29 @@ const SignUpModal: React.FC = () => {
           }
           value={password}
           onChange={onChangePassword}
+          onFocus={onFocusPassword}
+          useValidation
+          isValid={
+            !isPasswordHasNameOrEmail &&
+            isPasswordOverMinLength &&
+            !isPasswordHasNumberOrSymbol
+          }
+          errorMessage="비밀번호 입력하세요."
         />
       </div>
+      {passwordFocused && (
+        <>
+          <PasswordWarning
+            isValid={isPasswordHasNameOrEmail}
+            text="비밀번호에 본인 이름이나 이메일 주소를 포함할 수 없습니다."
+          />
+          <PasswordWarning isValid={!isPasswordOverMinLength} text="최소 8자" />
+          <PasswordWarning
+            isValid={isPasswordHasNumberOrSymbol}
+            text="숫자나 기호를 포함하세요."
+          />
+        </>
+      )}
       <p className="sign-up-birthdat-label">생일</p>
       <p className="sign-up-modal-birthday-info">
         만 18세 이상의 성인만 회원으로 가입할 수 있습니다. 생일은 다른
@@ -233,6 +269,7 @@ const SignUpModal: React.FC = () => {
             defaultValue="월"
             name="month"
             onChange={onChangeBirth}
+            isValid={!!birthMonth}
           />
         </div>
         <div className="sign-up-modal-birthday-day-selector">
@@ -242,6 +279,7 @@ const SignUpModal: React.FC = () => {
             defaultValue="일"
             name="day"
             onChange={onChangeBirth}
+            isValid={!!birthDay}
           />
         </div>
         <div className="sign-up-modal-birthday-year-selector">
@@ -251,14 +289,107 @@ const SignUpModal: React.FC = () => {
             defaultValue="년"
             name="year"
             onChange={onChangeBirth}
+            isValid={!!birthYear}
           />
         </div>
       </div>
       <div className="sign-up-modal-submit-button-wrapper">
         <Button type="submit">가입하기</Button>
       </div>
+      <p>
+        이미 에어비앤비 계정이 있나요?
+        <span
+          className="sign-up-modal-set-login"
+          role="presentation"
+          onClick={() => {}}
+        >
+          로그인
+        </span>
+      </p>
     </Container>
   );
 };
 
 export default SignUpModal;
+
+const Container = styled.div`
+  width: 568px;
+  padding: 32px;
+  background-color: white;
+  z-index: 11;
+
+  .mordal-close-x-icon {
+    cursor: pointer;
+    display: block;
+    margin: 0 0 40px auto;
+  }
+
+  .input-wrapper {
+    position: relative;
+    margin-bottom: 16px;
+    input {
+      position: relative;
+      width: 100%;
+      height: 46px;
+      padding: 0 44px 0 11px;
+      border: 1px solid ${palette.gray_eb};
+      border-radius: 4px;
+      font-size: 16px;
+      outline: none;
+      ::placeholder {
+        color: ${palette.gray_76};
+      }
+    }
+    svg {
+      position: absolute;
+      right: 11px;
+      top: 16px;
+    }
+  }
+
+  .sign-up-password-input-wrapper {
+    svg {
+      cursor: pointer;
+    }
+  }
+
+  .sign-up-birthdat-label {
+    font-size: 16px;
+    font-weight: 600;
+    margin-top: 16px;
+    margin-bottom: 8px;
+  }
+
+  .sign-up-modal-birthday-info {
+    margin-bottom: 16px;
+    color: ${palette.charcoal};
+  }
+
+  .sign-up-modal-birthday-selectors {
+    display: flex;
+    margin-bottom: 24px;
+    .sign-up-modal-birthday-month-selector {
+      margin-rignt: 16px;
+      flex-grow: 1;
+    }
+    .sign-up-modal-birthday-day-selector {
+      margin-right: 16px;
+      width: 25%;
+    }
+    .sign-up-modal-birthday-year-selector {
+      width: 33.3333%;
+    }
+  }
+
+  .sign-up-modal-submit-button-wrapper {
+    margin-bottom: 16px;
+    padding-bottom: 16px;
+    border-bottom: 1px solid ${palette.gray_eb};
+  }
+
+  .sign-up-modal-set-login {
+    color: ${palette.dark_cyan};
+    margin-left: 8px;
+    cursor: pointer;
+  }
+`;
