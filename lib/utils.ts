ae48905038e61f438f333707cf860df940b879db
ainochi-kor <kthrkdals@gmail.com>
2022-10-31 23:10:29 +0900

    add: price

--- lib/utils.ts
+++ lib/utils.ts
@@ -19,3 +19,32 @@ export const getNumber = (string: string) => {
   }
   return null;
 };
+
+//* 금액을 입력하면 금액에 ,를 넣어주는 함수
+export const makeMoneyString = (input: string) => {
+  const amountString = input.replace(/[^0-9]/g, "");
+  if (amountString) {
+    return parseInt(amountString, 10).toLocaleString();
+  }
+  return "";
+};
+
+//* query string 만들기
+export const makeQueryString = (
+  baseUrl: string,
+  queriesObject: Object & { [key: string]: any }
+) => {
+  const keys = Object.keys(queriesObject);
+  const values = Object.values(queriesObject);
+  if (keys.length === 0) {
+    return baseUrl;
+  }
+  let queryString = `${baseUrl}?`;
+  keys.forEach((key, i) => {
+    if (queriesObject[key]) {
+      queryString += `${keys[i]}=${values[i]}&`;
+    }
+  });
+  //* 마지막 '&' 제거하기
+  return queryString.slice(0, -1);
+};
